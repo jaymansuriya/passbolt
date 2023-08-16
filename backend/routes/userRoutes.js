8a865562ae8499c484a9ed6a835480defb11f4da
Jay Mansuriya <86514366+jaymansuriya@users.noreply.github.com>
2023-08-16 15:46:50 +0530

    Implemented update password route (#9)
    * Implemented update password route
    
    * Update userController.js

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -7,5 +7,6 @@ const router = Router();
 router.get("/", userController.getUser);
 router.put("/", userController.updateUser);
 router.delete("/", userController.deleteUser);
+router.post("/set_password", userController.updatePassword);
 
 module.exports = router;
