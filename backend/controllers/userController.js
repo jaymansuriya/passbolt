05b2c96b4c62a99dd362fc910797e12e0326091e
Jay mansuriya <jaydmansuriya@gmail.com>
2023-08-16 14:28:26 +0530

    Implemented update password route

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,11 +1,12 @@
 const Folder = require("../models/Folder");
 const Vault = require("../models/Vault");
 const User = require("../models/User");
+const bcrypt = require("bcrypt");
 
 const getUser = async (req, res) => {
   try {
     const userId = req.userId;
-    const user = await User.findById(userId).select({password: 0});
+    const user = await User.findById(userId).select({ password: 0 });
     if (!user) {
       return res.status(400).json({ error: "User not found" });
     }
@@ -23,7 +24,7 @@ const updateUser = async (req, res) => {
     const updatedUser = await User.findByIdAndUpdate(
       userId,
       { first_name, last_name, username, password, email },
-      { new: true, projection: { password: 0, _id: 0, __v: 0 } },
+      { new: true, projection: { password: 0, _id: 0, __v: 0 } }
     );
 
     if (!updatedUser) {
@@ -56,8 +57,44 @@ const deleteUser = async (req, res) => {
   }
 };
 
+const updatePassword = async (req, res) => {
+  try {
+    const { current_password, new_password, re_new_password } = req.body;
+    console.log(new_password.length + " password");
+    if (new_password.length < 6) {
+      return res
+        .status(400)
+        .json({ error: "New password must be at least 6 characters" });
+    }
+    if (new_password !== re_new_password) {
+      return res
+        .status(400)
+        .json({ error: "New passwords dose not match with Re-new password!" });
+    }
+
+    const user = await User.findOne({ _id: req.userId });
+
+    // Compare the provided current_password with the stored hashed password
+    const passwordMatch = await bcrypt.compare(current_password, user.password);
+    if (!passwordMatch) {
+      return res.status(401).json({ error: "Invalid current password" });
+    }
+
+    user.password = new_password;
+    console.log("Before DB================",user.password);
+    await user.save();
+    console.log("After DB===============",user.password);
+
+    return res.status(200).json({ message: "Password updated successfully" });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ error: "Internal server error" });
+  }
+};
+
 module.exports = {
   getUser,
   updateUser,
   deleteUser,
+  updatePassword,
 };
